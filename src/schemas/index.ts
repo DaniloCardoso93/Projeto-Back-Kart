3b65b235f8b9e485ca7f3b378c057dc049058da3
Danilo Cardoso <levitano@hotmail.com>
2023-04-10 11:01:31 -0300

    change default schema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -4,9 +4,3 @@ export const SchemaPadrao = {
     name:yup.string().required(),
     email:yup.string().required()
 }
-
-
-export const ReturnedPadrao = {
-    user:yup.object().shape(SchemaPadrao).required(),
-    updateAt:yup.date()
-}
