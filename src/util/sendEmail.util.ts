64ac128cbd8120c6acc20ad710f2be6769a724a8
Juan Sgarbi <juan.sgarbi1@hotmail.com>
2023-04-27 16:26:21 -0300

    fix bugns on sand email

--- src/util/sendEmail.util.ts
+++ src/util/sendEmail.util.ts
@@ -1,71 +1,76 @@
 import { createTransport } from "nodemailer";
 import { iEmailRequest } from "../interfaces/email.interface";
-import "dotenv/config"
+import "dotenv/config";
 import { AppError } from "../errors";
-import Mailgen from "mailgen"
+import Mailgen from "mailgen";
 
-const sendEmail = async({to, subject, text}: iEmailRequest) => {
+const sendEmail = async ({ to, subject, text }: iEmailRequest) => {
+  const transporter = createTransport({
+    host: "smtp.gmail.com",
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  });
 
-    const transporter = createTransport({
-        host:"smtp.gmail.com",
-        auth:{
-            user:process.env.SMTP_USER,
-            pass:process.env.SMTP_PASS
-        }
+  await transporter
+    .sendMail({
+      from: process.env.SMTP_USER,
+      to,
+      subject,
+      html: text,
     })
-
-    await transporter.sendMail({
-        from: "levitano96@gmail.com",
-        to,
-        subject,
-        html: text
-    }).then(() => {
-        console.log("email send with sucess")
-    }).catch((err) => {
-        console.log(err)
-        throw new AppError("Error sending email, try again later", 500)
+    .then(() => {
+      console.log("email send with sucess");
     })
-}
-
-
-const resetPasswordTemplete = (userEmail:string, userName:string, protocol:string, host:string, resetToken:string) => {
-    const mailGenerator = new Mailgen({
-        theme: 'default',
-        product: {
-            name: 'Reset Password',
-            link: `${protocol}://${host}`
-        }
+    .catch((err) => {
+      console.log(err);
+      throw new AppError("Error sending email, try again later", 500);
     });
+};
 
-    const email = {
-        body: {
-            name: userName,
-            intro: 'You have received this email because a password reset request for your account was received.',
-            action: {
-                instructions: 'Click the button below to reset your password:',
-                button: {
-                    color: '#DC4D2F',
-                    text: 'Reset your password',
-                    link: `${protocol}://${host}/users/resetPassword/${resetToken}`
-                }
-            },
-            outro: 'If you did not request a password reset, no further action is required on your part.'
-        }
-    };
+const resetPasswordTemplete = (
+  userEmail: string,
+  userName: string,
+  protocol: string,
+  host: string,
+  resetToken: string
+) => {
+  const mailGenerator = new Mailgen({
+    theme: "default",
+    product: {
+      name: "Reset Password",
+      link: `${protocol}://${host}`,
+    },
+  });
 
+  const email = {
+    body: {
+      name: userName,
+      intro:
+        "You have received this email because a password reset request for your account was received.",
+      action: {
+        instructions: "Click the button below to reset your password:",
+        button: {
+          color: "#DC4D2F",
+          text: "Reset your password",
+          link: `http://127.0.0.1:5173/resetPassword/${resetToken}`,
+        },
+      },
+      outro:
+        "If you did not request a password reset, no further action is required on your part.",
+    },
+  };
 
-    const emailBody = mailGenerator.generate(email)
+  const emailBody = mailGenerator.generate(email);
 
-    const emailTemplate = {
-        to:userEmail,
-        subject: "Reset password",
-        text: emailBody
-    }
+  const emailTemplate = {
+    to: userEmail,
+    subject: "Reset password",
+    text: emailBody,
+  };
 
-    return emailTemplate
-}
+  return emailTemplate;
+};
 
-
-
-
-export { sendEmail, resetPasswordTemplete }
+export { sendEmail, resetPasswordTemplete };
