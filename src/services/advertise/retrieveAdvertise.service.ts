5c98212a40ec491a6cd87f25bdb52ca79e580319
Juan Sgarbi <juan.sgarbi1@hotmail.com>
2023-04-28 12:37:53 -0300

    fix: fix announcement retrieve

--- src/services/advertise/retrieveAdvertise.service.ts
+++ src/services/advertise/retrieveAdvertise.service.ts
@@ -1,28 +1,29 @@
-import Announcement from "../../entities/announcement.entities"
-import { AppError } from "../../errors"
-import { announcementRepo } from "../../repositories"
-import { returnedAnnouncementShape } from "../../schemas/advertise.schema"
+import Announcement from "../../entities/announcement.entities";
+import { AppError } from "../../errors";
+import { announcementRepo } from "../../repositories";
+import { returnedAnnouncementShape } from "../../schemas/advertise.schema";
 
-const retrieveAdvertiseService = async (advertiseId:string):Promise<any> => {
-    
-    try {
-        const advertise = await announcementRepo
-        .createQueryBuilder("announcement")
-        .leftJoinAndSelect("announcement.user", "user")
-        .leftJoinAndSelect("announcement.images", "images")
-        .leftJoinAndSelect("announcement.comments", "comment")
-        .getOneOrFail()
+const retrieveAdvertiseService = async (advertiseId: string): Promise<any> => {
+  try {
+    const advertise = await announcementRepo
+      .createQueryBuilder("announcement")
+      .leftJoinAndSelect("announcement.user", "user")
+      .leftJoinAndSelect("announcement.images", "images")
+      .leftJoinAndSelect("announcement.comments", "comment")
+      .where("announcement.id = :id", { id: advertiseId })
+      .getOneOrFail();
 
-        const validateAnnouncement = await returnedAnnouncementShape.validate(advertise, {
-            stripUnknown:true
-        })
-    
-        return validateAnnouncement
-        
-    } catch (error) {
-        throw new AppError("Advertise not found!", 404)
-    }
-}
+    const validateAnnouncement = await returnedAnnouncementShape.validate(
+      advertise,
+      {
+        stripUnknown: true,
+      }
+    );
 
+    return validateAnnouncement;
+  } catch (error) {
+    throw new AppError("Advertise not found!", 404);
+  }
+};
 
-export default retrieveAdvertiseService
+export default retrieveAdvertiseService;
