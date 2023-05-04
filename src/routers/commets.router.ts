0fbc8d3f6783b54bebdd3be4aa8265ffaeb72f74
Danilo Cardoso <levitano@hotmail.com>
2023-05-04 11:38:14 -0300

    feat: CRUD Comments

--- src/routers/commets.router.ts
+++ src/routers/commets.router.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import { deleteCommentsController, getAllCommentsController, patchCommentsController, retrieveCommentsController } from "../controllers/comments.controller";
-import { ensureAuthMiddleware, validateSchemaMiddleware } from "../middlewares";
+import { ensureAuthMiddleware, ensureCommentParamsIdExistsMiddleware, ensureLoggerUserOwnerCommentMiddleware, validateSchemaMiddleware } from "../middlewares";
 import { registerCommentsShape } from "../schemas";
 
 export const commentRouter = Router()
 
 commentRouter.get("", ensureAuthMiddleware, getAllCommentsController)
 
-commentRouter.get("/:id", ensureAuthMiddleware, retrieveCommentsController)
+commentRouter.get("/:id", ensureCommentParamsIdExistsMiddleware, ensureAuthMiddleware, retrieveCommentsController)
 
-commentRouter.patch(":/id", ensureAuthMiddleware, validateSchemaMiddleware(registerCommentsShape), patchCommentsController)
+commentRouter.patch("/:id", validateSchemaMiddleware(registerCommentsShape), ensureAuthMiddleware, ensureCommentParamsIdExistsMiddleware, ensureLoggerUserOwnerCommentMiddleware, patchCommentsController)
 
-commentRouter.delete(":/id", ensureAuthMiddleware, deleteCommentsController)
+commentRouter.delete("/:id", ensureAuthMiddleware, ensureCommentParamsIdExistsMiddleware, ensureLoggerUserOwnerCommentMiddleware, deleteCommentsController)
--- src/services/comments/deleteComments.service.ts
+++ src/services/comments/deleteComments.service.ts
@@ -1,4 +1,17 @@
-const deleteCommentsService = async (commentId:string) => {}
+import Comment from "../../entities/comments.entities"
+import { AppError } from "../../errors";
+import { commentRepo } from "../../repositories"
+
+const deleteCommentsService = async (commentId:string) => {
+try {
+    commentRepo.delete({id:commentId})
+
+    return {}
+    
+} catch (error) {
+    throw new AppError("Comment not found!", 404);
+}
+}
 
 
 export default deleteCommentsService
